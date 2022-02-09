9098dfeec56b5bcba44ecfd452bceb9d905f83f2
Young-Chen Chang <ggteven@gmail.com>
2022-02-09 16:53:03 +0800

    modify style

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import Tab from "@material-ui/core/Tab";
 import AppBar from "@material-ui/core/AppBar";
 
 export default function App() {
-  const [page, setPage] = useState(1);
-  function handleChange(v, page) {
-    setPage(page);
-  }
+  const [value, setValue] = useState(0);
 
   return (
     <div className="App">
       <AppBar position="fixed">
-        <Tabs>
+        <Tabs value={value} onChange={(e, v) => setValue(v)}>
           <Tab label={"AsyncUpdate"} />
           <Tab label={"Context"} />
         </Tabs>
       </AppBar>
-      <div className="content">{page === 1 ? <AsyncUpdator /> : undefined}</div>
+      <div className="content">
+        {value === 0 ? <AsyncUpdator /> : undefined}
+      </div>
     </div>
   );
 }
